fda63324c69df3382add151f0413c10f8ebc3f9e
DELL-PC\DELL <myngoc3456@gmail.com>
2018-06-05 10:33:28 +0700

    Phần Admin Quản lý nhà sản xuất

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sanpham = require('./routes/sanpham');
 var loaisanpham = require('./routes/loaisanpham');
+var nhasanxuat = require('./routes/nhasanxuat');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', sanpham);
 app.use('/api', loaisanpham);
+app.use('/api',nhasanxuat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
