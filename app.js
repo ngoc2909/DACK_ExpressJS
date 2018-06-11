78a670197308b8d6c9a45cc011b7de01692a9a2a
DELL-PC\DELL <myngoc3456@gmail.com>
2018-06-11 10:54:37 +0700

    xây dựng api đăng nhập

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var jwt = require('jsonwebtoken');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +23,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+
+});
+
+var authenticateController=require('./controllers/authenticate-controller');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+/* route to handle login and registration */
+app.post('/api/authenticate',authenticateController.authenticate);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
