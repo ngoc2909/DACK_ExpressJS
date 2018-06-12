c338e0e4adad3912830ee592321cf299b7db3623
DELL-PC\DELL <myngoc3456@gmail.com>
2018-06-12 15:32:53 +0700

    Tạo api đăng ký

--- app.js
+++ app.js
@@ -32,12 +32,13 @@ app.use(function(req, res, next) {
 });
 
 var authenticateController=require('./controllers/authenticate-controller');
+var registerController=require('./controllers/register-controller');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 /* route to handle login and registration */
 app.post('/api/authenticate',authenticateController.authenticate);
-
+app.post('/api/register',registerController.register);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', sanpham);
