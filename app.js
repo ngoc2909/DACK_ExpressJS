94b51c4f081a4c10bc9b31cd77c23a13aaa91139
DELL-PC\DELL <myngoc3456@gmail.com>
2018-06-16 20:24:53 +0700

    Ràng buộc JWT đăng nhập

--- app.js
+++ app.js
@@ -4,6 +4,43 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var jwt = require('jsonwebtoken');
+var _ = require("lodash");
+var passport = require("passport");
+var passportJWT = require("passport-jwt");
+
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+var users = [
+    {
+        id: 1,
+        name: 'admin',
+        password: 'Ngoc29091996!'
+    },
+    {
+        id: 2,
+        name: 'user1',
+        password: 'Ngoc29091996!'
+    }
+];
+
+
+var jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = 'tasmanianDevil123';
+
+var strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+    next(null, jwt_payload)
+    // usually this would be a database call:
+    var user = users[_.findIndex(users, {id: jwt_payload.id})];
+    if (user) {
+        next(null, user);
+    } else {
+        next(null, false);
+    }
+});
+passport.use(strategy);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +51,8 @@ var taikhoan = require('./routes/taikhoan');
 var Users = require('./routes/User');
 
 var app = express();
+
+app.use(passport.initialize());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,6 +70,39 @@ app.use(function(req, res, next) {
     next();
 
 });
+
+app.post("/login", function(req, res) {
+    if(req.body.name && req.body.password){
+        var name = req.body.name;
+        var password = req.body.password;
+    }
+    // usually this would be a database call:
+    var user = users[_.findIndex(users, {name: name})];
+    if( ! user ){
+        res.status(401).json({message:"no such user found"});
+    }
+
+    if(user.password === req.body.password) {
+        // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+        var payload = {id: user.id};
+        var token = jwt.sign(payload, jwtOptions.secretOrKey);
+        res.json({message: "ok", token: token});
+    } else {
+        res.status(401).json({message:"passwords did not match"});
+    }
+});
+
+app.get("/secret", passport.authenticate('jwt', { session: false }), function(req, res){
+    res.json({message: "Success! You can not see this without a token"});
+});
+
+app.get("/secretDebug",
+    function(req, res, next){
+        console.log(req.get('Authorization'));
+        next();
+    }, function(req, res){
+        res.json("debugging");
+    });
 
 var authenticateController=require('./controllers/authenticate-controller');
 app.use('/User',Users);
